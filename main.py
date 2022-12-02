0c0cc57e362565cb78148a7f725e61964ec9349f
Aaron <asintayehu@proton.me>
2022-12-02 12:03:29 -0500

    added inorder traversal method, comments, and updated helper function with error checking

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ class BinaryTree(object):
         """
         if traversal_type == "preorder":
             return self.preorder_print(tree.root, "")
+        elif traversal_type == "inorder":
+            return self.inorder_print(tree.root, "")
         else:
             return f"traversal type {traversal_type} is not supported"
 
@@ -32,6 +34,23 @@ class BinaryTree(object):
             traversal = self.preorder_print(start.right, traversal)
         return traversal
 
+    def inorder_print(self, start, traversal):
+        """
+        Root -> Left -> Right
+        :param start:
+        :param traversal:
+        :return:
+        """
+        if start is not None:
+            traversal = self.inorder_print(start.left, traversal)
+            traversal += (str(start.value) + "-")
+            traversal = self.inorder_print(start.right, traversal)
+        return traversal
+
+    def postorder_print(self, start, traversal):
+
+
+
 
 
 
@@ -41,6 +60,8 @@ class BinaryTree(object):
 #     2         3
 #    /   \     /  \
 #   4     5   6    7
+#                 /
+#                8
 
 tree = BinaryTree(1)
 tree.root.left = Node(2)
@@ -49,7 +70,9 @@ tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
+# tree.root.right.right.left = Node(8)
 
-print(tree.print_tree("preorder"))
+# print(tree.print_tree("preorder"))
 # 1-2-4-5-3-6-7-
+print(tree.print_tree("inorder"))
 
